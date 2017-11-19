8e0fd11df25d0868190ce603f5be1b8fe006483e
Jo Pan <pan.huitong@yahoo.com>
2017-11-19 16:17:52 -0500

    Update finalproject_amazon_model.R

--- finalproject_amazon_model.R
+++ finalproject_amazon_model.R
@@ -1,3 +1,7 @@
+# SYS - Final Project
+# Amazon fine food review
+# https://www.kaggle.com/snap/amazon-fine-food-reviews
+
 #setwd('/home/yingjie/Desktop/SYS_Final_Proj')
 setwd('/Users/Pan/Google Drive/Data Science/SYS 6018')
 
@@ -6,26 +10,64 @@ library(MASS) #stepAIC
 
 data<-read.csv("modeldata.csv")
 colnames(data)[c(1,2)]<-c("id","help_int")
-#target: help_int
-#predictors: ~.-Id
+data$help_int<-factor(data$help_int)
 
+# create proper level names to prevent error for modeling
+feature.names=names(data)
+for (f in feature.names) {
+  if (class(data[[f]])=="factor") {
+    levels <- unique(c(data[[f]]))
+    data[[f]] <- factor(data[[f]],
+                         labels=make.names(levels))}
+}
+
+# split train and test set.
 set.seed(1)
-trainrows<-sample(1:nrow(data),size=1000)
+trainrows<-sample(1:nrow(data),size=10000)
+trainSet <- data[ trainrows,]
+testSet <- data[-trainrows,]
+
+# save outcome's name and predictors'names
+outcomeName<-'help_int'
+predictorsNames<-names(trainSet)[!names(trainSet) %in% c(outcomeName,'id')]
 
 ###################### Models ###################### ------------------------------------------------------------------------------------
-# Logistic regression (step)------------------------------------------------------------------------
+# Logistic regression - step (not able to run)------------------------------------------------------------------------
 glm1 <- glm(as.factor(help_int)~.-id, data=data[trainrows,], family = binomial(link = 'logit'))
-step <- stepAIC(glm1, direction="both")
+step <- stepAIC(glm1, direction="backward")
 step$anova
 
-# Logistic regression (caret)------------------------------------------------------------------------
-#https://rstudio-pubs-static.s3.amazonaws.com/43302_2d242dbea93b46c98ed60f6ac8c62edf.html
-fitControl <- trainControl(method = "cv",
-                           number = 5,
-                           ## Estimate class probabilities
-                           classProbs = TRUE,
-                           ## Evaluate performance using 
-                           ## the following function
-                           summaryFunction = twoClassSummary)
 
-glmBoostModel <- train(Class ~ ., data=trainData, method = "glmboost", metric="ROC", trControl = fitControl, tuneLength=5, center=TRUE, family=Binomial(link = c("logit")))
+# Logistic regression - step (caret) (bad) ------------------------------------------------------------------------
+model_glm<-train(trainSet[,predictors],trainSet[,outcomeName],
+                 trControl=objControl,method='glmStepAIC')
+
+#Plotting Variable importance for GLM
+plot(varImp(object=model_glm),main="GLM - Variable Importance")
+
+# gbm (boosting) (caret)------------------------------------------------------------------------
+# cross validation setting:
+objControl <- trainControl(method='cv', number=2, 
+                           returnResamp='none', summaryFunction = twoClassSummary, 
+                           classProbs = TRUE,verboseIter=FALSE,
+                           allowParallel= TRUE)
+
+# parameters optimizing:
+#     history: 
+#       1) with 1000 rows: "shrinkage = c(0.01,0.001)".        0.001 is much better.
+#                          "interaction.depth =  c(1, 5, 10)". 1 & 5 is better. 
+#                          "n.trees = c(100,500,1000)".       500 the best.slightly outperform 100 sometimes.
+#                                                             100 best among the rest.
+#                           best auc = 0.542
+gbmGrid <-  expand.grid(interaction.depth =  c(1, 5),
+                        n.trees = c(100,500,1000),
+                        shrinkage = c(0.001),
+                        n.minobsinnode=10)
+
+Model_gbm <- train(trainSet[,predictorsNames], trainSet[,outcomeName], 
+                  method='gbm', 
+                  trControl=objControl,  
+                  metric = "ROC",
+                  tuneGrid = gbmGrid)
+
+plot(Model_gbm)
